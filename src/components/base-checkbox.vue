6ea379e6704f51c200df9eb29c98c6a9f118e657
Y <Y@Y.com>
2019-05-30 22:42:03 +0800

    组件回忆

---
+++ src/components/base-checkbox.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <input
+        type="checkbox"
+       :checked="checked"
+        @change="$emit('change',$event.target.checked)"
+      >
+      {{isChecked}}
+      {{another}}
+      {{another2}}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'base-checkbox',
+  model: {
+    prop: 'another2',
+    event: 'change'
+  },
+  props: {
+    another2: Boolean,
+    isChecked: Boolean,
+    another: Boolean
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
