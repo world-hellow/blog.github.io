2940e59e72040ff55fab6ccbd11b75cee93eb9b5
Y <Y@Y.com>
2019-06-04 22:48:50 +0800

    basecompontent

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,14 +8,18 @@
         {{inputValue}}
         <BaseInput></BaseInput>
         <BaseInput v-model="inputValue"></BaseInput>
-        <BaseInput value="查看组件attrs属性" :attrs="2"></BaseInput>
+        <!--绑定属性到子组件上指定元素-->
+        <BaseInput value="右键查看组件attrs属性" :attrs="2"></BaseInput>
         <!--
            attrs="2" 默认会绑定到 子组件的跟元素上  div
            可以使用组件的  inheritAttrs: false  选项 解除跟元素的绑定
                           v-bind="$attrs"           绑定到指定元素上
            一个组件上的 v-model 默认会利用名为 value 的 prop 和名为 input 的事件，
            但是像单选框、复选框等类型的输入控件可能会将 value 特性用于不同的目的。
-           model 选项可以用来避免这样的冲突：
+           model 选项可以用来避免这样的冲突
+
+           子组件上添加原生事件
+           <base-input v-on:focus.native="onFocus"></base-input>
          -->
 
     </div>
